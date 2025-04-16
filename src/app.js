d46118958c581145c4bdbf00e73ee56048f37c9a
tawandakasunzuma <tskasunzuma@gmail.com>
2025-04-16 12:35:53 +0200

    add roadmap data and show sections with topics on the page

--- src/app.js
+++ src/app.js
@@ -1,7 +1,90 @@
-// 1. Load roadmap data
+// Roadmap data
+const roadmapData = [
+    {
+        sectionName: "HTML",
+        listOfTopics: [
+            {
+                title: "HTML Basics",
+                checked: false,
+                notes: ""
+            }
+        ]
+    },
+    {
+        sectionName: "CSS",
+        listOfTopics: [
+            {
+                title: "CSS Fundamentals",
+                checked: false,
+                notes: "" 
+            },
+            {
+                title: "Flexbox & Grid",
+                checked: false,
+                notes: ""
+            },
+            {
+                title: "Responsive Design",
+                checked: false,
+                notes: ""
+            }
+        ]
+    },
+    {
+        sectionName: "JavaScript",
+        listOfTopics: [
+            {
+                title: "JavaScript Basics",
+                checked: false,
+                notes: ""
+            },
+            {
+                title: "DOM Manipulation",
+                checked: false,
+                notes: ""
+            },
+            {
+                title: "Using Fetch API",
+                checked: false,
+                notes: ""
+            },
+            {
+                title: "ES6+ Features",
+                checked: false,
+                notes: ""
+            }
+        ]
+    },
+    {
+        sectionName: "Frameworks $ Tooling",
+        listOfTopics: [
+            {
+                title: "Tooling (npm, webpack, etc.)",
+                checked: false,
+                notes: ""
+            },
+            {
+                title: "Basic React",
+                checked: false,
+                notes: ""
+            }
+        ]
+    }
+]
 
-// 2. Render each topic dynamically
+// Content container
+const content = document.getElementById("content");
 
-// 3. Track checkbox changes
+// Loop through roadmap data
+for (let i = 0; i < roadmapData.length; i++) {
+    const section = document.createElement("section");
 
-// 4. Save to localStorage
+    const sectionHeading = document.createElement("h3");
+    sectionHeading.innerHTML = roadmapData[i].sectionName;
+    sectionHeading.classList.add("section-heading");
+
+    const cardList = document.createElement("div");
+
+    section.append(sectionHeading,cardList);
+    content.append(section)
+}
